e4eb9f57048db57c4c74fc6c963e4b039fd495f5
subiran <subiran.garcia.ana@gmail.com>
2015-03-10 01:11:39 +0100

    p02_v2

--- app/src/main/java/com/example/ana/p02_fotos/p02.java
+++ app/src/main/java/com/example/ana/p02_fotos/p02.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class p02 extends Activity {
+public class p02 extends Activity implements detectaImg {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
